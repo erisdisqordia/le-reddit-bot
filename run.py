d05d4f496605b19a8e98a78d8d59e4ce55908b1e
Luna Mendes <lkmnds@gmail.com>
2018-05-01 02:26:26 -0300

    working posts

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import requests
+import mimetypes
 
 import requests
 from mastodon import Mastodon
@@ -11,11 +12,62 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-def poll_toot(mastodon):
-    mastodon.toot(f'testing! timestamp {time.time()}')
-
+def _do_res(last_post):
     cur_t = time.time()
-    return cur_t, cur_t + config.TOOT_PERIOD
+    return cur_t, cur_t + config.TOOT_PERIOD, last_post
+
+
+def poll_toot(mastodon, last_post):
+    log.info('calling reddit...')
+    resp = requests.get(config.REDDIT_URL, headers={
+        'User-Agent': 'eunvr/0.1'
+    })
+    log.info(repr(resp))
+
+    if resp.status_code != 200:
+        log.error(f'Status code is not 200: {resp.status}')
+        log.error(resp)
+        return _do_res(last_post)
+
+    try:
+        log.info('requesting json...')
+        data = resp.json()
+        assert isinstance(data, dict)
+    except:
+        log.exception('Failed to parse json.')
+        return _do_res(last_post)
+
+    data = data['data']
+    children = data['children']
+
+    child = data['children'][0]
+    child_data = child['data']
+    child_id = child_data['id']
+
+    log.info(f'id to post: {child_id}, last id: {last_post}')
+
+    if child_id == last_post:
+        log.warning('last posted == current child, ignoring')
+        return _do_res(last_post)
+
+    # this has the image
+    child_url = child_data['url']
+    image = requests.get(child_url)
+
+    # upload image
+    mimetype, _encoding = mimetypes.guess_type(child_url)
+    log.info(f'sending image (mimetype: {mimetype})...')
+
+    media = mastodon.media_post(image.content, mimetype)
+
+    log.info('sending toot...')
+    toot = mastodon.status_post(f'{child_data["title"]} '
+                                f'https://redd.it/{child_data["id"]}',
+                                media_ids=[media['id']], sensitive=True)
+
+    log.info(f'sent! new id is: {toot["id"]}')
+
+    return _do_res(toot['id'])
 
 
 def main():
@@ -33,7 +85,7 @@ def main():
         next_tstamp, current_tstamp = map(float, fd.read().split(','))
         fd.close()
     except FileNotFoundError:
-        next_tstamp, current_tstamp = None, None
+        next_tstamp, current_tstamp, last_post = None, None, None
 
     while True:
         log.debug(f'next: {next_tstamp}, current: {current_tstamp}')
@@ -41,17 +93,17 @@ def main():
         # first time running OR .eu_nvr_last_toot is lost
         if current_tstamp is None:
             # poll right now, then schedule the next one
-            current_tstamp, next_tstamp = poll_toot(mastodon)
+            current_tstamp, next_tstamp, last_post = poll_toot(mastodon, last_post)
 
         current_tstamp = time.time()
 
         # we have current_tstamp, good, is it beyond next_tstamp?
         if current_tstamp > next_tstamp:
-            current_tstamp, next_tstamp = poll_toot(mastodon)
+            current_tstamp, next_tstamp, last_post = poll_toot(mastodon, last_post)
 
         # after doing those, update the file!
         with open(config.BOT_STATE, 'w') as f:
-            f.write(f'{next_tstamp},{current_tstamp}')
+            f.write(f'{next_tstamp},{current_tstamp},{last_post}')
 
         # wait a second before checking those again!
         time.sleep(1)
