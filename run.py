21dd20c1b900132f57bded3e1d95dd3ff47b6a45
Luna <git@l4.pm>
2019-11-10 15:01:20 -0300

    add retry logic when requests call fails
     - run black fmt

--- run.py
+++ run.py
@@ -17,9 +17,10 @@ TSTAMP_NEXT = 1
 
 
 def _update_timestamp(cur, ttype, tstamp):
-    cur.execute('delete from timestamps where ttype=?', (ttype,))
-    cur.execute('insert into timestamps (ttype, timestamp) values (?, ?)',
-                (ttype, tstamp))
+    cur.execute("delete from timestamps where ttype=?", (ttype,))
+    cur.execute(
+        "insert into timestamps (ttype, timestamp) values (?, ?)", (ttype, tstamp)
+    )
 
 
 def _do_res(conn):
@@ -37,7 +38,7 @@ def _do_res(conn):
 
 def _fetch_tstamp(conn, ttype) -> int:
     cur = conn.cursor()
-    cur.execute('select timestamp from timestamps where ttype=?', (ttype,))
+    cur.execute("select timestamp from timestamps where ttype=?", (ttype,))
     return cur.fetchone()
 
 
@@ -60,22 +61,22 @@ def fetch_cur_tstamp(conn) -> int:
 def fetch_last_posts(conn) -> list:
     """Fetch tooted posts from db"""
     cur = conn.cursor()
-    cur.execute('select postid from posts')
+    cur.execute("select postid from posts")
     last_posts = cur.fetchall()
     return [e[0] for e in last_posts]
 
 
 def is_image(child) -> bool:
     """Check if a post object has an image attached."""
-    child_data = child['data']
-    child_url = child_data['url']
-    return child_url.endswith(('.jpg', '.jpeg', '.png', '.gif'))
+    child_data = child["data"]
+    child_url = child_data["url"]
+    return child_url.endswith((".jpg", ".jpeg", ".png", ".gif"))
 
 
 def not_posted(child, conn) -> bool:
     """Check if a post has been already tooted."""
-    child_data = child['data']
-    child_id = child_data['id']
+    child_data = child["data"]
+    child_id = child_data["id"]
 
     last_posts = fetch_last_posts(conn)
 
@@ -84,14 +85,14 @@ def not_posted(child, conn) -> bool:
 
 def is_nsfw(child) -> bool:
     """return if a post is set to over 18 (NSFW)."""
-    data = child['data']
-    return data.get('over_18', False)
+    data = child["data"]
+    return data.get("over_18", False)
 
 
 def gen_cw_text(child_data: dict) -> bool:
     """Generate a content warning text for the post
     based on the flair or title of the post."""
-    flair = child_data.get('link_flair_text')
+    flair = child_data.get("link_flair_text")
 
     if flair in config.ACCEPTED_FLAIRS:
         return flair
@@ -99,7 +100,7 @@ def gen_cw_text(child_data: dict) -> bool:
     # match configurable content warnings
     # with the post's title
     for cw_title, keywords in config.CONTENT_WARNINGS.items():
-        title = child_data['title']
+        title = child_data["title"]
 
         # search for any match
         if any(word in title for word in keywords):
@@ -109,87 +110,106 @@ def gen_cw_text(child_data: dict) -> bool:
     # can be set to a default CW by using an empty keyword
 
 
-def poll_toot(mastodon, conn):
+def poll_toot(mastodon, conn, retry_count=0):
     """Query reddit and toot if possible."""
-    log.info('calling reddit...')
+    log.info("calling reddit...")
 
-    resp = requests.get(config.REDDIT_URL, headers={
-        # reddit blocks me when I have a requests useragent
-        # lol.
-        'User-Agent': 'eunvr/0.1'
-    })
+    try:
+        resp = requests.get(
+            config.REDDIT_URL,
+            headers={
+                # reddit blocks me when I have a requests useragent
+                # lol.
+                "User-Agent": "eunvr/0.1"
+            },
+        )
+    except:
+        log.exception(
+            "Failed to request given reddit url. retrying (try %d)", retry_count
+        )
+
+        if retry_count > 5:
+            log.error("retried too much")
+            raise RuntimeError("retried too much")
+
+        time.sleep(1)
+        retry_count += 1
+        return poll_toot(mastodon, conn, retry_count)
 
     if resp.status_code != 200:
-        log.error(f'Status code is not 200: {resp.status}')
+        log.error("Status code is not 200: %d", resp.status_code)
         log.error(resp)
         _do_res(conn)
         return
 
     try:
-        log.info('requesting json...')
+        log.info("requesting json...")
         data = resp.json()
         assert isinstance(data, dict)
     except Exception:
-        log.exception('Failed to parse json.')
+        log.exception("Failed to parse json.")
         _do_res(conn)
         return
 
-    data = data['data']
+    data = data["data"]
 
     last_posts = fetch_last_posts(conn)
 
     # find all posts that have images, weren't tooted and aren't nsfw.
-    useful_children = (child for child in data['children']
-                       if is_image(child) and not_posted(child, conn) and
-                       not is_nsfw(child))
+    useful_children = (
+        child
+        for child in data["children"]
+        if is_image(child) and not_posted(child, conn) and not is_nsfw(child)
+    )
 
     try:
         child = next(useful_children)
     except StopIteration:
-        log.error('no useful children found')
+        log.error("no useful children found")
         _do_res(conn)
         return
 
-    child_data = child['data']
-    child_id = child_data['id']
+    child_data = child["data"]
+    child_id = child_data["id"]
 
-    log.info(f'id to post: {child_id}, last ids: {last_posts}')
+    log.info(f"id to post: {child_id}, last ids: {last_posts}")
 
     if child_id in last_posts:
-        log.warning('last posted == current child, ignoring')
+        log.warning("last posted == current child, ignoring")
         _do_res(conn)
         return
 
-    child_url = child_data['url']
+    child_url = child_data["url"]
     image = requests.get(child_url)
 
     # upload image since we can't pass the URL
     mimetype, _encoding = mimetypes.guess_type(child_url)
-    log.info(f'sending image (mimetype: {mimetype})...')
+    log.info(f"sending image (mimetype: %r)...", mimetype)
     try:
         media = mastodon.media_post(image.content, mimetype)
     except MastodonAPIError:
-        log.exception('error while sending image, ignoring')
+        log.exception("error while sending image, ignoring")
 
         # since media failed to upload, its best we ignore
         # the post altogether by inserting it into db
-        conn.execute('insert into posts (postid) values (?)', (child_id,))
+        conn.execute("insert into posts (postid) values (?)", (child_id,))
         conn.commit()
 
         _do_res(conn)
         return
 
-
-    log.info('sending toot...')
+    log.info("sending toot...")
     cw_text = gen_cw_text(child_data)
-    toot = mastodon.status_post(f'{child_data["title"]} '
-                                f'https://redd.it/{child_data["id"]}',
-                                media_ids=[media['id']], sensitive=True,
-                                spoiler_text=cw_text)
+    toot = mastodon.status_post(
+        f'{child_data["title"]} ' f'https://redd.it/{child_data["id"]}',
+        media_ids=[media["id"]],
+        sensitive=True,
+        spoiler_text=cw_text,
+    )
 
     log.info(f'sent! toot id: {toot["id"]}, post id: {child_id!r}')
 
-    conn.execute('insert into posts (postid) values (?)', (child_id,))
+    conn.execute("insert into posts (postid) values (?)", (child_id,))
     conn.commit()
 
     _do_res(conn)
@@ -204,7 +224,8 @@ def main():
     )
 
     db = sqlite3.connect(config.BOT_STATE)
-    db.executescript("""
+    db.executescript(
+        """
     create table if not exists posts (
         postid text not null
     );
@@ -213,7 +234,8 @@ def main():
         ttype int,
         timestamp bigint
     );
-    """)
+    """
+    )
 
     while True:
         current_tstamp = fetch_cur_tstamp(db)
@@ -237,11 +259,11 @@ def main():
         if int(current_tstamp) % 100 == 0:
             remaining = next_tstamp - current_tstamp
             remaining = round(remaining, 5)
-            log.info(f'{remaining} seconds before poll time')
+            log.info(f"{remaining} seconds before poll time")
 
         # wait a second before doing it all again
         time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
